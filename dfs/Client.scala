6b8284bf22684faed6a1f32c4937cbf6412f2145
Mark Toujiline <mark.toujiline@gmail.com>
2020-11-27 19:55:14 -0800

    Thread pool, send binary data

--- dfs/Client.scala
+++ dfs/Client.scala
@@ -4,19 +4,51 @@ package dfs
 import java.net._
 import java.io._
 import scala.io._
-
-
+import scala.annotation.tailrec
 
 class Client(port: Int) {
     def run(): Unit = {
         val s = new Socket(InetAddress.getByName("localhost"), port)
-        lazy val in = new BufferedSource(s.getInputStream()).getLines()
-        val out = new PrintStream(s.getOutputStream())
+        val remoteIn = new BufferedSource(s.getInputStream())
+        val file = new BufferedInputStream(new FileInputStream("in.txt"))
+        val out = new BufferedOutputStream(s.getOutputStream())
 
-        out.println("Hello, world")
+        @tailrec
+        def upload(): Unit = {
+            /**
+              * Since underlying InputStream is FileInputStream,
+              * a -1 is written when EOF has been reached.
+              */
+            val byte = file.read()
+
+            if (byte != -1) {
+                out.write(byte)
+                upload()
+            }
+        }
+
+        upload()
+
+        /**
+         * Flush is necessary because OutputStream is buffered.
+         * If buffer hasn't been filled up, then data isn't sent.
+         * Flush sends whatever is in the buffer, even if it isn't full.
+         */
+        
         out.flush()
-        println("Received: " + in.next())
 
+        /**
+         * Closes output side of the socket. Input is still open
+         * so messages from server can be receives. No more data
+         * can be sent to server via this connection.
+         */
+        s.shutdownOutput()
+
+        println("Read response from server, if any:")
+        for (line <- remoteIn.getLines()) {
+            println(s"::: $line")
+        }
+        file.close()
         s.close()
     }
 }
--- dfs/Server.scala
+++ dfs/Server.scala
@@ -3,74 +3,88 @@ package dfs
 import java.net._
 import java.io._
 import scala.io._
+import scala.annotation.tailrec
+
+class ServerThread(serverSocket: ServerSocket) extends Thread {
+
+    /**
+     * Because getLines blocks, there is no way for the interrupt to terminate the
+     * thread without some data being sent. The only way to stop the thread is to 
+     * close the socket. This will cause a SocketException to be thrown at the getLines
+     * call site, which is why that exception has to be handled.
+     */
+    override def interrupt(): Unit = {
+        serverSocket.close()
+        super.interrupt()
+    }
+
+    override def run(): Unit = {
+        try {
+            while (!Thread.interrupted()) {
+                /**
+                 * Block until connection is made.
+                 */
+                val socket = serverSocket.accept()
+                println(s"Server port created for client = ${socket.getPort()} by ${Thread.currentThread().getName()}")
+                /**
+                  * In/out streams represent a stream of bytes.
+                  * Implemented read/write operations read/write a single
+                  * byte at a time. Blocks until data is available
+                  * or end of stream is detected.
+                  */
+                val inputStream = socket.getInputStream()
+                val outputStream = socket.getOutputStream()
+
+                /**
+                  * BufferedSource and PrintStream are wrappers around lower level Stream objects.
+                  * Provide convenience methods.
+                  */
+                val out = new PrintStream(outputStream)
+
+                @tailrec
+                def readData(): Unit = {
+                    val byte = inputStream.read()
+                    if (byte != -1) {
+                        os.write.append(os.pwd/"out.txt", Array(byte.byteValue()))
+                        readData()
+                    }
+                }
+                readData()
+                out.println("Finished uploading")
+                socket.close()
+                println("Closed socket")
+            }
+        } catch {
+            case ex: SocketException =>
+                println("Socket was closed, terminating gracefully.")
+            case ex: IOException => {
+                println("Socket#accept failed")
+                ex.printStackTrace()
+            }
+            case ex: SecurityException =>
+                println("Security manager did not allow operation")
+                ex.printStackTrace()
+        }
+    }
+}
 
 class Server(port: Int) {
 
     def run(): Unit = {
-        val serverThread = new Thread {
-            /**
-              * Creates a socket bound to the port. At this point, other
-              * apps can no longer create sockets at the same port. Also,
-              * connections are not allowed until `accept` is called.
-              */
-            val server = new ServerSocket(port);
-
-            /**
-              * Because getLines blocks, there is no way for the interrupt to terminate the
-              * thread without some data being sent. The only way to stop the thread is to 
-              * close the socket. This will cause a SocketException to be thrown at the getLines
-              * call site, which is why that exception has to be handled.
-              */
-            override def interrupt(): Unit = {
-                server.close()
-                super.interrupt()
-            }
-
-            override def run(): Unit = {
-                try {
-                    while (!Thread.interrupted()) {
-                        /**
-                          * Block until connection is made.
-                          */
-                        val socket = server.accept()
-
-                        /**
-                          * In/out streams represent a stream of bytes.
-                          * Implemented read/write operations read/write a single
-                          * byte at a time. Blocks until data is available
-                          * or end of stream is detected.
-                          */
-                        val inputStream = socket.getInputStream()
-                        val outputStream = socket.getOutputStream()
-
-                        /**
-                          * BufferedSource and PrintStream are wrappers around lower level Stream objects.
-                          * Provide convenience methods.
-                          */
-                        val in = new BufferedSource(inputStream)
-                        val out = new PrintStream(outputStream)
-
-                        val it: Iterator[String] = in.getLines()
-                        
-                        // Send next line to socket recipient
-                        out.println(s"server-${it.next()}")
-                    }
-                } catch {
-                    case ex: SocketException =>
-                        println("Socket was closed, terminating gracefully.")
-                    case ex: IOException => {
-                        println("Socket#accept failed")
-                        ex.printStackTrace()
-                    }
-                    case ex: SecurityException =>
-                        println("Security manager did not allow operation")
-                        ex.printStackTrace()
-                }
-            }
+        /* Creates a socket bound to the port. At this point, other
+         * apps can no longer create sockets at the same port. Also,
+         * connections are not allowed until `accept` is called.
+        */
+        val socket = new ServerSocket(port)
+        val threadPool = for(i <- 1 to 3) yield {
+            val thread = new ServerThread(socket)
+            thread.setName(s"Thread-${i}")
+            thread
         }
-        serverThread.start()
+
+        threadPool.foreach(_.start())
         println("Press enter to stop server ")
         StdIn.readLine()
-        serverThread.interrupt()
+        threadPool.foreach(_.interrupt())
     }
 }
